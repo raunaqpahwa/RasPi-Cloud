147b912737400cb9d974f7d058c8d86d8d27a452
raunaqpahwa <raunaqpahwa@gmail.com>
2020-04-06 15:44:31 +0530

    Added concurrently

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
 import axios from "axios";
 import HomePage from "./components/HomePage";
 import appStyles from "./App.module.css";
 function App() {
   const [isAuthenticated, setAuthentication] = useState(false);
+  useEffect(() => {
+    const isAuthenticated = sessionStorage.getItem("authenticated");
+    setAuthentication(isAuthenticated);
+  }, []);
   function handleAuth(username, password) {
     axios
       .post("/authenticate", { username, password })
-      .then(response => {
+      .then((response) => {
         const { authenticated } = response.data;
         if (authenticated) {
           setAuthentication(true);
+          sessionStorage.setItem("authenticated", "true");
         }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
   return (
     <div className={appStyles.appDiv}>
